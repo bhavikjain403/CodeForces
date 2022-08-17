176ea862e4e15c2663a1d7cdaee395b9cf0926ef
Bhavik Jain <bhavikjain403@gmail.com>
2022-08-17 15:10:07 +0530

    Day 45

---
+++ 1433A.py
@@ -0,0 +1,15 @@
+t=int(input())
+while t:
+    x=input()
+    l=len(x)
+    if l==4:
+        print((int(x[0])-1)*10+10)
+    elif l==3:
+        print((int(x[0])-1)*10+6)
+    elif l==2:
+        print((int(x[0])-1)*10+3)
+    else:
+        print((int(x[0])-1)*10+1)
+    t-=1
+
+22
