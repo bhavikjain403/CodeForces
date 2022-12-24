0bb33104747cafbc7fb3fb8b150ee9dba415ef43
Bhavik Jain <bhavikjain403@gmail.com>
2022-12-24 10:36:50 +0530

    Day 169

---
+++ 1375C.py
@@ -0,0 +1,14 @@
+class solve:
+    def __init__(self):
+        for i in range(int(input())):
+            self.solution()
+ 
+    def solution(self):
+        n=int(input())
+        a=list(map(int,input().split()))
+        if a[0]<a[n-1]:
+            print("YES")
+        else:
+            print("NO")
+ 
+obj=solve()
