c5555246b72f5a84f37a370d4898e6eee62fa89d
Bhavik Jain <bhavikjain403@gmail.com>
2023-02-06 19:32:20 +0530

    Day 213

---
+++ 743A.py
@@ -0,0 +1,10 @@
+class solve:
+    def __init__(self):
+        n,a,b=map(int,input().split())
+        s=input()
+        if s[a-1]==s[b-1]:
+            print(0)
+        else:
+            print(1)
+ 
+obj=solve()
