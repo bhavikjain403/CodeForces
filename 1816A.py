f61e289b5c09c100017d45db9bcd780b4c4cdf71
Bhavik Jain <bhavikjain403@gmail.com>
2023-04-12 19:31:18 +0530

    Day 278

---
+++ 1816A.py
@@ -0,0 +1,12 @@
+class solve:
+    def __init__(self):
+        for i in range(int(input())):
+            self.solution()
+ 
+    def solution(self):
+        a,b=map(int,input().split())
+        print(2)
+        print(a-1,1)
+        print(a,b)
+ 
+obj=solve()
