91ca789c4904414b6409a8d3602e477ec29c7099
Bhavik Jain <bhavikjain403@gmail.com>
2023-04-13 14:05:24 +0530

    Day 279

---
+++ 1728A.py
@@ -0,0 +1,11 @@
+class solve:
+    def __init__(self):
+        for i in range(int(input())):
+            self.solution()
+ 
+    def solution(self):
+        n=int(input())
+        a=list(map(int,input().split()))
+        print(a.index(max(a))+1)
+ 
+obj=solve()
