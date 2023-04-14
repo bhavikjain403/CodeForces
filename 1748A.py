739157bf9cf6ecd9804665208ebe6b96d90e51f4
Bhavik Jain <bhavikjain403@gmail.com>
2023-04-14 23:04:21 +0530

    Day 280

---
+++ 1748A.py
@@ -0,0 +1,10 @@
+class solve:
+    def __init__(self):
+        for i in range(int(input())):
+            self.solution()
+ 
+    def solution(self):
+        n=int(input())
+        print((n+1)//2)
+ 
+obj=solve()
